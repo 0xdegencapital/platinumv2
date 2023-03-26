861f7f12afd8690f3769d9e54612fd0d952c0391
zerorekt <brilliantedgar@gmail.com>
2023-03-26 23:27:49 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,105 @@
+import { MenuEntry } from 'platinumfinancev2'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Parallel farming',
+    icon: 'HamburgerIcon',
+    items: [
+      {
+        label: 'Iridium Finance',
+        href: 'https://iridiumfinance.app/',
+      },
+    ],
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://quickswap.exchange/#/swap?outputCurrency=0x782eb3304F8b9adD877F13a5cA321f72c4AA9804',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://quickswap.exchange/#/add/0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270/0x782eb3304F8b9adD877F13a5cA321f72c4AA9804',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  // {
+  //   label: 'Community Pools',
+  //   icon: 'GroupsIcon',
+  //   href: '/community',
+  // },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Charts',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'QuickChart',
+        href: 'https://quickchart.app/token/0x782eb3304F8b9adD877F13a5cA321f72c4AA9804',
+      },
+      {
+        label: 'PolyChart',
+        href: 'https://app.polychart.io/explorer/polygon/0x8c79d1ccb5e5ea4df5e0ff14b59a305f2808ac93',
+      },
+      {
+        label: 'DexGuru',
+        href: 'https://dex.guru/token/0x782eb3304f8b9add877f13a5ca321f72c4aa9804-polygon',
+      },
+      {
+        label: 'PooCoin',
+        href: 'https://polygon.poocoin.app/tokens/0x782eb3304f8b9add877f13a5ca321f72c4aa9804',
+      },
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      // {
+      //   label: 'Github',
+      //   href: 'https://github.com/polydaedalus',
+      // },
+      {
+        label: 'Docs',
+        href: 'https://platinum-finance.gitbook.io/platinum-finance/',
+      },
+      {
+        label: 'Vfat.tools',
+        href: 'https://vfat.tools/polygon/platinumfinance/',
+      }
+    ],
+  },
+]
+
+export default config
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,42 @@
+export default {
+  cake: {
+    137: '0x782eb3304F8b9adD877F13a5cA321f72c4AA9804' 
+  },
+  masterChef: {
+    137: '0x1E37A59cB7dA60c0b4273169eade2126ADf6cd7E',
+  },
+  sousMasterChef: {
+    137: '0x563373d51Eb8e9F9074216763a39F3aD79597edd'
+  },
+  weth: {
+    137: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    137: '0x95028E5B8a734bb7E2071F96De89BABe75be9C8E'
+  },
+  usdc: {
+    137: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+  },
+  usdt: {
+    137: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', // USDT
+    80001: '',
+  },
+  wbtc: {
+    137: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', // wbtc
+    80001: '',
+  },
+  wmatic: {
+    137: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+  },
+  matic: {
+    137: '0x0000000000000000000000000000000000001010'
+  }
+}
